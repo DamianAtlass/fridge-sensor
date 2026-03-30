36bb9832cb4b3a399557c6fb05d4e5bb6ac0ff25
DamianAtlass <damianatlass@gmx.de>
2026-03-30 17:49:06 +0200

    removed redundency

--- src/distance_sensor.py
+++ src/distance_sensor.py
@@ -14,4 +14,5 @@ def take_measurment():
     v = (readChannel(0) / 1023.0) * 3.3
     return 16.2537 * v ** 4 - 129.893 * v ** 3 + 382.268 * v ** 2 - 512.611 * v + 301.439
 
-print("Taken measurement: ", take_measurment())
+if __name__ == "__main__":
+    print("Taken measurement: ", take_measurment())
